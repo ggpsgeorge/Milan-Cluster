6539f21934aa394bfc1a3dd2f5d60635377e9193
ggpsgeorge <ggpsgeorge@gmail.com>
2021-04-24 08:48:02 -0300

    Activities are added in the layer

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 let mapid = document.getElementById("mapid");
 
-console.log(mapid);
-
 let mymap = L.map(mapid).setView([45.4678, 9.1914], 13);
-// let mymap = L.map('mapid').setView([37, -109.05], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox/streets-v11',
@@ -12,16 +9,31 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoiZ2dwc2dlb3JnZSIsImEiOiJja2xoNjRoNnk1YnRnMnJwbGhjdjdkMW9lIn0.kn_ZuIZt6PkfprwNnUPRwg'
 }).addTo(mymap);
 
-// AQUI SANSIRO MESMO COM AS LAT E LONG TROCADAS E 5 COORDENADAs AO INVES DE 4 FUNCIONA
 $.getJSON("dados\\geojsons\\2013-11-03.geojson", function(data) {
-    data['features'].forEach(element => {
+    // console.log(data["features"][2045]["properties"]["activity"]);
+    data["features"].forEach(element => {
         L.geoJSON(element["geometry"], {
             color: element["properties"]["stroke"],
             fillColor: element["properties"]["fill"],
             fillOpacity: element["properties"]["fill-opacity"],
             opacity: element["properties"]["stroke-opacity"],
             weight: element["properties"]["stroke-width"],
+            activity: element["properties"]["activity"],
+            onEachFeature: onEachFeature
         }).addTo(mymap);
     });
 })
+
+let anomaly_container = document.createElement("div");
+anomaly_container.classList.add("anomaly-container");
+let graph = document.createElement("img");
+graph.src = "jojomeme.jpg";
+anomaly_container.appendChild(graph);
+document.body.appendChild(anomaly_container);
+
+function onEachFeature(feature, layer){
+    layer.on('click', function(){
+        console.log(layer)        
+    })
+}
 
